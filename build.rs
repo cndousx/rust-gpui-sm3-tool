d88cbb553075b53a4e5384da720609c36f607978
DouShaoxun <dousx_cn@163.com>
2026-06-30 09:19:01 +0800

    resource

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "windows" {
         let mut res = winresource::WindowsResource::new();
         // 设置图标
-        res.set_icon("app.ico");
+        res.set_icon("resources/app.ico");
 
         res.compile().unwrap();
     }
